e484e00e18c5a62566cc705f1fa364af6671cbd6
Evan Kalaw <e.c.kalaw@gmail.com>
2016-07-20 12:23:38 +0000

    almost working commit

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -15,39 +15,6 @@ class LinkedList(AbstractLinkedList):
         if self.elements:
             for elem in self.elements:
                 self.append(elem)
-
-        #############################################
-        # DON'T NEED ANY OF THIS.......
-        # self.nodeList = []                
-        #     if len(self.elements) == 1:
-        #       newNode = Node(self.elements[0])
-        #       self.start = newNode
-        #       self.end = newNode
-        #     if len(self.elements) > 1:
-        #         for index in range(len(elements)):
-        #             self.noded_list.append(Node(self.elements[index]))
-        #         self.start = self.noded_list[0]
-        #         self.end = self.noded_list[-1]
-        #         for index in range(len(self.noded_list)-1):
-        #             self.noded_list[index].next = self.noded_list[index+1]
-    
-    # def __init__(self, elements=None):
-    #     self.elements = elements
-    #     self.start = None
-    #     self.end = None
-    #     self.noded_list = []
-    #     if elements:
-    #         for index in range(len(self.elements)):
-    #             if index == 0:
-    #                 new_node = Node(self.elements[index],self.end)
-    #                 self.noded_list.append(new_node)
-    #                 self.start = new_node
-    #             else:
-    #                 new_node = Node(self.elements[index],self.end)
-    #                 self.noded_list.append(new_node)
-    #                 self.noded_list[index-1].next=new_node
-    #                 self.end = new_node
-    ##################################################################
     
     def __str__(self):
         if self.start:
@@ -85,7 +52,6 @@ class LinkedList(AbstractLinkedList):
     def __iadd__(self, other):
         self.append(LinkedList(other).start)
 
-
     def __eq__(self, other):
         return str(self) == str(other)
     
@@ -98,41 +64,25 @@ class LinkedList(AbstractLinkedList):
             self.end.next = appNode
             self.end = appNode
 
-        # self.noded_list.append(addedNode)
-        # if len(self.noded_list) < 2:
-        #     self.start = addedNode
-        #     self.end = addedNode
-        # else:
-        #     self.end = addedNode
-        #     self.noded_list[-2].next = self.end
-
-        # if self.start == None:
-        #     self.start = addedNode
-        # if self.end != None:
-        #     self.end.next = addedNode
-        # self.end = addedNode
-        # self.noded_list[-1].next = 
-
     def count(self):
         return len(self)
 
     def pop(self, index=None):
         maxIndex = self.count() - 1
+        if index:
+            poppedNode = self.__getitem__(index)
         if index >=0 and index <= maxIndex:
             self.end = self.__getitem__(index + 1)
             self.__getitem__(index - 1).next = self.end
-        elif index == maxIndex: #last index
+        elif index == maxIndex: #last index C: added the case when they dont enter an index to pop  or index == None
             self.end = self.__getitem__(index - 1)
             self.end.next = None
         else:
-            raise
-
-    # # our own methods defined down here (for now)
-    # def start(self):
-    #     pass
-    
-    # def end(self):
-    #     return self[-1].next
+            index = maxIndex
+            poppedNode = self.__getitem__(index).elem
+            self.end = self.__getitem__(index - 1)
+            self.__getitem__(index - 1).next
+        return poppedNode
         
 
 ########## RUN ME PLZ #############
